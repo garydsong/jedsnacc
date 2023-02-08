b0e34152dca31d9907793d630081c553db81ddb7
garydsong <ceouls@gmail.com>
2023-02-07 21:57:41 -0800

    added dispatch

--- react-app/src/components/Snack/SnackBox.js
+++ react-app/src/components/Snack/SnackBox.js
@@ -3,9 +3,11 @@ import { useSelector } from "react-redux";
 import "../Snack/SnackBox.css"
 import SnackPage from "./SnackPage";
 import { getAllSnacksThunk } from "../../store/snack";
+import { useDispatch } from "react-redux";
 
 
 export default function SnackBox() {
+    const dispatch = useDispatch()
     const snacks = useSelector(state => state.snacks.allSnacks)
 
     useEffect(() => {
